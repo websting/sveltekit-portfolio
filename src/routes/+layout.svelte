647862caeb807b86bc67e274442ae84c40aac5a2
websting <40836693+websting@users.noreply.github.com>
2023-03-10 20:49:59 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 
 <style>
     :global(body){
-        background-image: url(../gradient.jpg);
+        background-image: url(https://images.unsplash.com/photo-1523821741446-edb2b68bb7a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80);
         color: lightgrey;
         background-size: cover;
         background-repeat: no-repeat;
